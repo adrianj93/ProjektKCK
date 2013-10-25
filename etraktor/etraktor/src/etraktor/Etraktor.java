a7bfa70c311c103beeee083893c6bf0a06c7b8b6
adrianj93 <adrianj93@gmail.com>
2013-10-25 08:53:28 +0200

    25.10

---
+++ etraktor/etraktor/src/etraktor/Etraktor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etraktor;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Etraktor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FrameEkranGlowny().setVisible(true);
+    }
+}
