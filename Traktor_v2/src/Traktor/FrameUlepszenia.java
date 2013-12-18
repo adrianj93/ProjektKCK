af7885561627ac4d3b2343d520072dd94fb39eba
Łukasz <fracek144@gmail.com>
2013-12-18 20:52:42 +0100

    updejto

--- Traktor_v2/src/Traktor/FrameUlepszenia.java
+++ Traktor_v2/src/Traktor/FrameUlepszenia.java
@@ -6,7 +6,7 @@ public class FrameUlepszenia extends javax.swing.JFrame {
         initComponents();
     }
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings("checked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
